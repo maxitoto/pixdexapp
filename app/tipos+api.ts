ddda15d7f244bfb7620d87f096fa7828e7b305e2
maxitoto <tommyush2000@hotmail.com>
2025-06-09 02:54:43 -0300

    problema "FALLO TypeError: Network request failed" solo desde expo Go

--- app/tipos+api.ts
+++ app/tipos+api.ts
@@ -1,5 +1,6 @@
 import { tiposContenidoAudiovisual } from "@/src/constants/Data/tiposContenidoAudiovisual"; 
 
 export function GET(response: Response){
+    console.log('GET function executed');
     return Response.json(tiposContenidoAudiovisual);
 }
--- src/screens/home/components/CategoryList.tsx
+++ src/screens/home/components/CategoryList.tsx
@@ -1,13 +1,48 @@
-import {tiposContenidoAudiovisual} from "@/src/constants/Data/tiposContenidoAudiovisual";
+import {ITipoContenidoAudiovisual, tiposContenidoAudiovisual} from "@/src/constants/Data/tiposContenidoAudiovisual";
 import { View, FlatList, StyleSheet } from "react-native";
 import { CardAudioVisualList } from "./CardAudioVisualList"
+import { API_URL } from "@/src/constants/urls"
+import { useEffect, useState } from "react";
+
+async function getTipos(): Promise<ITipoContenidoAudiovisual[]> {
+  console.log(`${API_URL}/tipos`);
+  const responseTipos = await fetch(`${API_URL}/tipos`);
+
+  if (!responseTipos.ok) {
+    throw new Error("Error al obtener tipos");
+  }
+  console.log("-----------");
+  const tipos: ITipoContenidoAudiovisual[] = await responseTipos.json();
+  console.log(tipos);
+  
+  return tipos
+}
+
+
 
 export function CategoryList(){
-    
+    const [tipos, setTipos] = useState<ITipoContenidoAudiovisual[]>([]);
+
+    useEffect(() => {
+        obtenerTipos();
+    }, []);
+
+    async function obtenerTipos() {
+        try {
+        const tiposDict = await getTipos();
+        setTipos(tiposDict);
+        } catch (error) {
+        alert(`FALLO ${error}`);
+        console.log("ERROR", error);
+        } finally {
+        }
+    }
+
+  
     return(
         <View style={styles.containerList}>
             <FlatList
-                data={tiposContenidoAudiovisual}
+                data={tipos}
                 keyExtractor={(tipo) => tipo.id.toString()}
                 renderItem={({ item }) => <CardAudioVisualList {...item} />}
                 contentContainerStyle={styles.separador}
